6ceeeea88474383f4cb016e272886242132e9b7e
warrenarea <warrenarea@gmail.com>
2014-04-09 17:24:23 -0700

    Runs Client Server to Receive streamed data.

---
+++ RunClient.bat
@@ -0,0 +1,8 @@
+echo off
+PATH=C:\Python27\;C:\Python27\Scripts;
+echo The Render EEG Graph requires PyGame to run.
+echo Plug in Emotiv USB device. 
+pause
+echo on
+Python.exe .\Python\client.py
+pause
