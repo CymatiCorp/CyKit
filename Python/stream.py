0d062e58d034db52df1c8ec92c5c4e5bcb9ab937
warrenarea <warrenarea@gmail.com>
2014-12-31 18:16:21 -0800

    Update stream.py
    Added <IP> and <PORT> arguments, example: python.exe stream.py localhost 5555
    
    Added better handling of sockets.
    
    Fixed the crashing after connecting multiple times.
    
    will now close greenlets and allow to reconnect.
    
    I did have it set up (unintentionally) to receive multiple connections at once, which started
    multiple greenlets that sent data to the client. However this was taxing after a couple of
    connections, and not what I was looking for.

--- Python/stream.py
+++ Python/stream.py
@@ -1,47 +1,71 @@
 from EmotKit import emotiv
 import gevent
 import socket
+import sys
 
 def main():
-    headset = emotiv.Emotiv()
-    gevent.spawn(headset.setup)
-    connectServer()
-    gevent.sleep(1)
+
+    try:
+         connectServer()
+    except Exception, e:
+         print(e)
+
+    gevent.sleep(0.001)
     try:
        while True: 
              packet = headset.dequeue()
              connbuffer = ""
 
              for name in 'AF3 F7 F3 FC5 T7 P7 O1 O2 P8 T8 FC6 F4 F8 AF4'.split(' '):
-              connbuffer += " " +  str(packet.sensors[name]['value']) 
-             conn.send(connbuffer)
-            #conn.sendall(connbuffer + b'\0')  
-            #This sends 14 sensors on one terminating line but packet data is skipped.
+              connbuffer +=  str(packet.sensors[name]['value'])  + "."
+
+             conn.sendall(connbuffer + '\n')
              connbuffer = ""
-    except KeyboardInterrupt:
+    except Exception as msg:
+             print 'Error: ' + str(msg)
              conn.close()
-             headset.close()
+             #headset.kill()
     finally:
+             print "Disconnecting . . ."
              conn.close()
-             headset.close()
+             gevent.GreenletExit
+             print "Restarting . . ."
+             main()
+
 
 def connectServer():
      global conn
-     HOST = 'localhost'
-     PORT = 21013
-     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     s.bind((HOST, PORT))
-     s.listen(1)
+     HOST = str(sys.argv[1])
+     PORT = int(sys.argv[2])
+     print "Connecting to " + HOST + " : " + str(PORT)
+     try:
+         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     except socket.error as msg:
+         s = None
+
+     try:
+         s.bind((HOST, PORT))
+         s.listen(1)
+     except socket.error as msg:
+         s.close()
+         s = None
+
+     if s is None:
+         print 'Could not open socket'
+         return
+     
+
      conn, addr = s.accept()
      print 'Connected to', addr
 
 
 try:
-  print "Connect to 127.0.0.1 : 21013"
+  headset = emotiv.Emotiv()
+  gevent.spawn(headset.setup)
   main()
-  
+
 except Exception, e:
   print e
-  print "Device Time Out or Disconnect . . . Reconnect to Server."
+  print "Device Time Out or Disconnect . . .    Reconnect to Server."
   conn.close()
   main()
