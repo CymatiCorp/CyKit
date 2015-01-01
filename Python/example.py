60e7ea6a273f23270d89b43530519b98ac39533d
warrenarea <warrenarea@gmail.com>
2015-01-01 07:26:05 -0800

    Update example.py

--- Python/example.py
+++ Python/example.py
@@ -1,14 +1,19 @@
-from CyKit import emotiv
+import sys
+sys.path.insert(0, './Python/CyKit/')
+import emotiv
 import gevent
+import socket
+del socket
+del sys
 
 if __name__ == "__main__":
-    headset = emotiv.Emotiv()
+    headset = emotiv.Emotiv(False, False)
     gevent.spawn(headset.setup)
     gevent.sleep(1)
     try:
         while True:
             packet = headset.dequeue()
-            print packet.gyroX, packet.gyroY
+            print packet.gyro_x, packet.gyro_y
             gevent.sleep(0)
     except KeyboardInterrupt:
         headset.close()
