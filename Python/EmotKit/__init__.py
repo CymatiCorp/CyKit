68b6404f1d89bcf653d8c9956d5b686a5370334b
warrenarea <warrenarea@gmail.com>
2014-12-31 17:19:28 -0800

    Update __init__.py

--- Python/EmotKit/__init__.py
+++ Python/EmotKit/__init__.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-emokit - emotiv epoc reverse engineering
+CyKit
 ~~~~~~~~~~~~~~~
 
-:copyright: (c) 2013 by ???
-:license: ???
+:copyright: (c) 2015 CymatiCorp
+:license: Public
 """
 
-__title__ = "EmotKit"
+__title__ = "CyKit"
 __version__ = "1.0"
-__build__ = 0x000001
-__author__ = "Bryan Bishop <kanzure@gmail.com>"
-__license__ = "???"
-__copyright__ = "Copyright 2013 Bryan Bishop"
+__build__ = 0x000002
+__author__ = "Bryan Bishop, Severin Lemaignan, Bill Schumacher, Sharif Olorin, Warren"
+__license__ = "Public"
+__copyright__ = " (c) 2015 CymatiCorp"
 
 from . import emotiv
-from . import mouse_control
