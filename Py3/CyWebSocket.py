bd8ce2e38cfbbd667636fce423d75464849b421b
CymatiCorp <warrenarea@gmail.com>
2023-03-01 05:53:17 -0800

    Update (Fix) : deprecation for base64 encodestring.
    Replaced base64 encodestring function, for new standard:   base64.b64encode()
    
    Deprecation suggests to use:  base64.encodebytes(bytes object)
    
    as suggested by: mhrahmani
    Suggested to replace with: base64.b64encode()
    however, that does not include a \n line-feed byte at the end.
    
    our code removes the \n byte anyways, so we go with b64encode()

--- Py3/CyWebSocket.py
+++ Py3/CyWebSocket.py
@@ -183,8 +183,7 @@ class socketIO():
                                 unit = data.split(": ")
                                 header[unit[0]] = unit[1]
                         secKey = header['Sec-WebSocket-Key']
-                        resKey = base64.encodebytes(hashlib.new("sha1",(secKey+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11").encode('utf-8')).digest())
-                        resKey = resKey.decode().replace("\n","")
+                        resKey = base64.b64encode(hashlib.new("sha1",(secKey+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11").encode('utf-8')).digest())
                         response = '''HTTP/1.1 101 Switching Protocols\r\n'''
                         response += '''Upgrade: websocket\r\n'''
                         response += '''Connection: Upgrade\r\n'''
